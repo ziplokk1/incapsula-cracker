c0cab6f80cb418538861736745adbc40bd28e43b
Mark Sanders <ziplokk.mark.sanders@gmail.com>
2016-04-12 23:54:28 -0500

    Fixing

--- incapsula/scrapy_.py
+++ incapsula/scrapy_.py
@@ -32,7 +32,7 @@ class IncapsulaMiddleware(object):
         return cookie
 
     def process_response(self, request, response, spider):
-        print 'processing %s' % request.url
+        print 'processing %s - for %s' % (request.url, request.meta.get('org_req_url'))
         if not request.meta.get('incap_set', False):
             soup = BeautifulSoup(response.body_as_unicode())
             meta = soup.find('meta', {'name': 'robots'})
