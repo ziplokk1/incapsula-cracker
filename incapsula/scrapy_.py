1abc2524b37dd46605e1ce10d6c64fce606deffe
Mark Sanders <ziplokk.mark.sanders@gmail.com>
2016-04-12 23:34:32 -0500

    Working on scrapy

---
+++ incapsula/scrapy_.py
@@ -0,0 +1,65 @@
+import random
+import time
+
+from scrapy import Request
+from BeautifulSoup import BeautifulSoup
+
+from methods import *
+
+
+class IncapsulaMiddleware(object):
+
+    cookie_count = 0
+
+    def _get_session_cookies(self, request):
+        cookies_ = []
+        for cookie_key, cookie_value in request.cookies.items():
+            if 'incap_ses_' in cookie_key:
+                cookies_.append(cookie_value)
+        return cookies_
+
+    def get_incap_cookie(self, request, response):
+        extensions = load_plugin_extensions(navigator['plugins'])
+        extensions.append(load_plugin(navigator['plugins']))
+        extensions.extend(load_config())
+        cookies = self._get_session_cookies(request)
+        digests = []
+        for cookie in cookies:
+            digests.append(simple_digest(",".join(extensions) + cookie))
+        res = ",".join(extensions) + ",digest=" + ",".join(str(digests))
+        cookie = create_cookie('___utmvc', res, 20, request.url)
+        return cookie
+
+    def process_response(self, request, response, spider):
+        soup = BeautifulSoup(response.body)
+        meta = soup.find('meta', {'name': 'robots'})
+        if not meta:
+            return response
+        if not request.meta.get('incap_set', False):
+            cookie = self.get_incap_cookie(request, response)
+            scheme, host = urlparse.urlsplit(request.url)[:2]
+            url = '{scheme}://{host}/_Incapsula_Resource?SWKMTFSR=1&e={rdm}'.format(scheme=scheme, host=host, rdm=random.random)
+            return Request(url, meta={'incap_set': True}.update(request.meta), cookies=[cookie])
+        if request.meta.get('incap_set', False):
+            timing = []
+            start = now_in_seconds()
+            timing.append('s:{}'.format(now_in_seconds() - start))
+
+            code = get_obfuscated_code(response.body)
+            parsed = parse_obfuscated_code(code)
+            resource1, resource2 = get_resources(parsed, response.url)[1:]
+            return Request(resource1, cookies=request.cookies, meta={
+                'resource2': resource2,
+                'tstart': start,
+                'timing': timing,
+                'incap_request_1': True
+            }.update(request.meta))
+        if request.meta.get('incap_request_1', False):
+            timing = request.meta.get('timing', [])
+            resource2 = request.meta.get('resource2')
+            start = request.meta.get('tstart')
+            timing.append('c:{}'.format(now_in_seconds() - start))
+            time.sleep(0.02)
+            timing.append('r:{}'.format(now_in_seconds() - start))
+            return Request(resource2 + urllib.quote('complete ({})'.format(",".join(timing))), cookies=request.cookies)
+        return response
